dbf6c1530e836a890fe5e81ed96ffa0226fe760a
hugosenari <hugosenari@gmail.com>
2022-03-22 00:38:50 -0300

    chore: re-index ../nimpkgs/u/uncomment

--- main/flake.nix
+++ main/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-uncomment-main.flake = false;
   inputs.src-uncomment-main.owner = "hamidb80";
-  inputs.src-uncomment-main.ref   = "refs/heads/main";
+  inputs.src-uncomment-main.ref   = "main";
   inputs.src-uncomment-main.repo  = "uncomment";
   inputs.src-uncomment-main.type  = "github";
   
